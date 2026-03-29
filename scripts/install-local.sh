ddf3903e809e457b52b232fb33d9574930aa56ef
Lucas Caovilla <lucasgrisac@gmail.com>
2026-03-29 11:13:42 -0300

    fix: Fixes install.sh.

--- scripts/install-local.sh
+++ scripts/install-local.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+VERSION=$(grep -oP '(?<=<Version>)[^<]+' src/BaseDDD.Cli/BaseDDD.Cli.csproj)
+
 rm -Rf ./nupkg ./obj
-dotnet pack -c Release -o ./nupkg
+dotnet pack src/BaseDDD.Cli/BaseDDD.Cli.csproj -c Release -o ./nupkg
 dotnet tool uninstall -g baseddd.cli || true
-dotnet tool install -g --add-source ./nupkg baseddd.cli
+dotnet tool install -g --add-source ./nupkg baseddd.cli --version "$VERSION"
+
+echo "Installed baseddd version $VERSION"
